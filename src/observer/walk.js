cd337c67fa1af86cf99105abf19a17e982a78f37
wangaoxin <wangaoxin@lvjitec.com>
2020-08-10 10:45:57 +0800

    fix: bug in walk obj

--- src/observer/walk.js
+++ src/observer/walk.js
@@ -9,6 +9,7 @@ function doWalk(obj){
   if(!obj || (!Array.isArray(obj) && typeof obj !== 'object') || touchedObjs.includes(obj)){
     return;
   }
+  touchedObjs.push(obj);
   if(Array.isArray(obj)){
     obj.forEach(v => doWalk(v));
   }else{
